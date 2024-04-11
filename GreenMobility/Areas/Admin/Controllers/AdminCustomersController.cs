fcc46a21a41e4c1bd0de89ed768a5989109f6ee2
becdanh <lvietbaodanh@gmail.com>
2024-04-11 11:28:08 +0700

    init feature for AdminCustomers

---
+++ GreenMobility/Areas/Admin/Controllers/AdminCustomersController.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GreenMobility.Models;
+using AspNetCoreHero.ToastNotification.Abstractions;
+using PagedList.Core;
+
+namespace GreenMobility.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminCustomersController : Controller
+    {
+        private readonly GreenMobilityContext _context;
+        private readonly INotyfService _notyf;
+        public AdminCustomersController(GreenMobilityContext context, INotyfService notyf)
+        {
+            _context = context;
+            _notyf = notyf;
+        }
+
+        // GET: Admin/AdminCustomers
+        public async Task<IActionResult> Index(int page = 1, int status = 0, string keyword = "")
+        {
+            var pageNumber = page;
+            var pageSize = 10;
+
+            List<SelectListItem> lsStatus = new List<SelectListItem>();
+            lsStatus.Add(new SelectListItem() { Text = "Tất cả trạng thái", Value = "0" });
+            lsStatus.Add(new SelectListItem() { Text = "Hoạt động", Value = "1" });
+            lsStatus.Add(new SelectListItem() { Text = "Khóa", Value = "2" });
+            ViewData["lsStatus"] = lsStatus;
+
+            IQueryable<Customer> customerQuery = _context.Customers
+                .AsNoTracking();
+
+            if (status == 2)
+            {
+                customerQuery = customerQuery.Where(x => x.IsLocked);
+            }
+            else if (status == 1)
+            {
+                customerQuery = customerQuery.Where(x => !x.IsLocked);
+            }
+
+            if (!string.IsNullOrEmpty(keyword))
+            {
+                keyword = keyword.Trim().ToLower();
+                customerQuery = customerQuery.Where(x => x.FullName.ToLower().Contains(keyword) || x.Email.ToLower().Contains(keyword));
+            }
+
+            List<Customer> lsCustomers = await customerQuery.ToListAsync();
+            PagedList<Customer> models = new PagedList<Customer>(lsCustomers.AsQueryable(), pageNumber, pageSize);
+
+            ViewBag.CurrentPage = pageNumber;
+            ViewBag.CurrentStatus = status;
+            ViewBag.Keyword = keyword;
+
+            return View(models);
+        }
+
+        public IActionResult FilterAndSearch(int status = 0, string keyword = "")
+        {
+            var url = $"/Admin/AdminCustomers?status={status}&keyword={keyword}";
+            if (status == 0 && string.IsNullOrEmpty(keyword))
+            {
+                url = "/Admin/AdminCustomers";
+            }
+            return Json(new { status = "success", redirectUrl = url });
+        }
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customers
+                .FirstOrDefaultAsync(m => m.CustomerId == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return View(customer);
+        }
+
+        // GET: Admin/AdminCustomers/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/AdminCustomers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CustomerId,FullName,Phone,Email,Address,Password,Salt,CreateDate,LastLogin,IsLocked")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(customer);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(customer);
+        }
+
+        // GET: Admin/AdminCustomers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customers.FindAsync(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: Admin/AdminCustomers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CustomerId,FullName,Phone,Email,Address,Password,Salt,CreateDate,LastLogin,IsLocked")] Customer customer)
+        {
+            if (id != customer.CustomerId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(customer);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CustomerExists(customer.CustomerId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(customer);
+        }
+
+        // GET: Admin/AdminCustomers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customers
+                .FirstOrDefaultAsync(m => m.CustomerId == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return View(customer);
+        }
+
+        // POST: Admin/AdminCustomers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var customer = await _context.Customers.FindAsync(id);
+            if (customer != null)
+            {
+                _context.Customers.Remove(customer);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CustomerExists(int id)
+        {
+            return _context.Customers.Any(e => e.CustomerId == id);
+        }
+    }
+}
